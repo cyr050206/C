7f524c5678c973befdb6749033df3974b89448e2
CyrusRumao <cyrus.rumao006@gmail.com>
2023-11-06 00:25:27 +0530

    bruh

--- Assignments/smallBigfn.c
+++ Assignments/smallBigfn.c
@@ -2,20 +2,20 @@
 #include<conio.h>
 void max(int a, int b)
 {
-    a>b?(printf("%d is greater\n", a)):(printf("%d is greater\n", b));
+    a > b ? (printf("%d is greater\n", a)) : (printf("%d is greater\n", b));
 }
 void min(int a, int b)
 {
-    a<b?(printf("%d is smaller\n", a)):(printf("%d is smalller\n", b));
+    a < b ? (printf("%d is smaller\n", a)) : (printf("%d is smalller\n", b));
 }
 int main()
 {
-    int a ,b;
+    int a, b;
     printf("Enter 1st number: ");
     scanf("%d", &a);
     printf("Enter 2nd number: ");
     scanf("%d", &b);
-    max(a,b);
-    min(a,b);
+    max(a, b);
+    min(a, b);
     getch();
 }
