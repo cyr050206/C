39c056c5b219b64b650f4d9b8125bb8982ac17c2
CyrusRumao <cyrus.rumao006@gmail.com>
2023-11-22 21:44:32 +0530

    Prime Numbers

---
+++ C Programs/PrimeRecursion.c
@@ -0,0 +1,58 @@
+/*#include<stdio.h>
+#include<conio.h>
+
+int check(int n, int p)
+{
+    if (n % p == 1)
+    {
+
+        return 1;
+
+    else
+        return check(n, p - 1);
+}
+
+
+int main()
+{
+    int num1;
+    int x = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num1);
+    x = check(num1, (num1 - 1));
+    if (x == 1)
+        printf("Prine");
+    else
+        printf("No prime");
+    return 0;
+}*/
+
+#include <stdio.h>
+
+int isPrime(int num, int i) {
+    if (i == 1) {
+        return 1;
+    }
+    else {
+        if (num % i == 0) {
+            return 0;
+        }
+        else {
+            return isPrime(num, i - 1);
+        }
+    }
+}
+int main() {
+    int num, check;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    check = isPrime(num, num / 2);
+    if (check == 1) {
+        printf("%d is a prime number\n", num);
+    }
+    else {
+        printf("%d is not a prime number\n", num);
+    }
+    return 0;
+}
+
--- Practice/Arraysstrings.c
+++ Practice/Arraysstrings.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include<String.h>
-int main(){
+int main() {
     char c;
-    char cars[][10] = {"Ford", "Mustang", "Camero"};
-    strcpy(cars[0], "Ferrari");
-    for(int i = 0; i< sizeof(cars)/ sizeof(cars[0]); i++)
+    char s[10];
+    char cars[][10] = { "Ford", "Mustang", "Camero" };
+    scanf("%s", &s);
+    strcpy(cars[0], s);
+    for (int i = 0; i < sizeof(cars) / sizeof(cars[0]); i++)
     {
         printf("%s \n", cars[i]);
     }
